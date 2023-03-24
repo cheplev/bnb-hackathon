4468701c010be492c3ec57fe54572a34264a1e2f
chep <vcheplev42@gmail.com>
2023-03-24 17:23:04 +0400

    migrate to polygon

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config({ path: ".env" });
+require("dotenv").config();
 
-const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-/** @type import('hardhat/config').HardhatUserConfig */
+const { API_URL, PRIVATE_KEY } = process.env;
+//* Default Template for Reference
+
 module.exports = {
   solidity: "0.8.19",
+  defaultNetwork: "polygon_mumbai",
   networks: {
-    goerli: {
-      url: QUICKNODE_HTTP_URL,
-      accounts: [PRIVATE_KEY],
+    hardhat: {
+      chainId: 31337
     },
-  },
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  }
 };
+
+
+// Configuration
+/*
+  solidity - The version of solidity compiler
+  defaultNetwork - The Default network to run (Without running --network-name)
+  networks - Object which contains the network information
+  etherscan - Object to fill in EtherScan Information for contract verification
+*/
+// module.exports = {
+//   solidity: "0.8.9",
+// };
